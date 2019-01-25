1ec5bc9880fb5537f5c5a7ec6240e8e51e68334d
Ashima161991 <ashima.chawla16@gmail.com>
2019-01-25 21:43:37 +0000

    Add files via upload

---
+++ IMDB.py
@@ -0,0 +1,105 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+##import the libraries 
+
+from keras.datasets import imdb
+from keras import models
+from keras import layers
+from keras import optimizers
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# In[2]:
+
+
+# Load the IMDB data
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(
+    num_words=10000)#argument num_words=10000 means you’ll only keep the top 10,000 most frequently occurring words
+#train_data[0]
+#train_labels[0]
+
+## One hot encode the input
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))        
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.                          
+    return results
+
+x_train = vectorize_sequences(train_data)                  
+x_test = vectorize_sequences(test_data) 
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+
+###########validation set by setting apart 10,000 samples from the original training data
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+
+# In[4]:
+
+
+## Keras Sequential Model
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer=optimizers.RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=20,
+                    batch_size=512,
+                    validation_data=(x_val, y_val))
+
+results = model.evaluate(x_test, y_test)
+results
+
+
+# In[7]:
+
+
+history_dict = history.history
+history_dict.keys()
+
+
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+acc=history_dict['acc']
+
+
+epochs = range(1, len(acc) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')           
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')      
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
+acc_values = history_dict['acc']
+
+val_acc= history_dict['val_acc']
+
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.show()
+
